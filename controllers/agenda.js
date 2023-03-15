89d6fcfb273378721f0af8413cd5ceb640bdc79c
Joseph Herrera <josephherreraleon@gmail.com>
2023-03-15 08:04:41 -0600

    Creacion de agenda y recordatorios

--- controllers/agenda.js
+++ controllers/agenda.js
@@ -1,6 +1,25 @@
+const Recordatorio = require("../models/recordatorios");
+
 exports.getAgenda = (req, res, next) => {
     res.render("agenda/main", {
       tituloPagina: "Agenda",
       ruta: "/main",
     });
   };
+
+exports.getNota = (req, res, next) => {
+  res.render("agenda/nota", {
+    tituloPagina: "Nota",
+    ruta: "/nota",
+  });
+};
+
+exports.getRecordatorio = (req, res, next) => {
+  Recordatorio.mostrarTodo((recordatorios) => {
+    res.render("agenda/recordatorio", {
+      tituloPagina: "Recordatorio",
+      ruta: "/recordatorio",
+      recordatorios: recordatorios,
+    });
+  });
+};
--- routes/agenda.js
+++ routes/agenda.js
@@ -5,5 +5,7 @@ const agendaControl = require('../controllers/agenda');
 const router = express.Router();
 
 router.get('/main', agendaControl.getAgenda);
+router.get('/nota', agendaControl.getNota);
+router.get('/recordatorio', agendaControl.getRecordatorio);
 
 module.exports = router;
